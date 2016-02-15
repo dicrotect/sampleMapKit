57c71ea9a010947c06ca8def23a41d08c7e6ef46
dicrotect <dicrotect9050@gmail.com>
2016-02-15 15:15:12 +0800

    swiftでのMapKitの練習です

--- sampleMapKit/ViewController.swift
+++ sampleMapKit/ViewController.swift
@@ -7,17 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //MAPの指定をする
+        //MAPの座標を決めるオブジェクトを作成
+        
+        let coodinate = CLLocationCoordinate2DMake(40.139439,141.431)
+        //表示の縮尺を指定
+        let span = MKCoordinateSpanMake(0.04, 0.04)
+        
+        //範囲オブジェクトを作成
+        let reigion = MKCoordinateRegionMake(coodinate, span)
+        
+        myMapView.setRegion(reigion, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
